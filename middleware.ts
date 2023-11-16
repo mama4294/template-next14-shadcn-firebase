99f9d0d5a7cac4f371f5715d6618fae64bcbfa13
mama4294 <matthew.malone@colorado.edu>
2023-11-15 17:23:38 -0700

    adds comments

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth;
+export default withAuth; //https://next-auth.js.org/configuration/nextjs#middleware
 
 export const config = {
   //https://nextjs.org/docs/app/building-your-application/routing/middleware
