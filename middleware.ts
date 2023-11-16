428cb38b26059c5350779d4796fd44abb5e48057
mama4294 <matthew.malone@colorado.edu>
2023-11-15 17:22:50 -0700

    fixes footer

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,7 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth;
+
 export const config = {
   //https://nextjs.org/docs/app/building-your-application/routing/middleware
   matcher: ["/protectedroute"], //routes that require the user to be logged in
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ function Footer() {
   return (
     <footer className="flex bg-background h-16 w-full items-center justify-between px-4 text-xs sm:text-sm">
       <div>Matthew Malone</div>
-      <div className="flex">
+      <div className="flex gap-1">
         <p>Copyright © {currentYear}</p>
-        <p>All right reserved</p>
+        <p>All rights reserved</p>
       </div>
     </footer>
   );
